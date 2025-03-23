655b48f028ce7fb168d6100e90066bdfec120938
Danejw <48073085+Danejw@users.noreply.github.com>
2025-03-22 17:59:24 -1000

    add witty response agent and irony definition functionality

--- app/my_agents.py
+++ app/my_agents.py
@@ -1,5 +1,5 @@
 import asyncio
-from agents import Agent, Runner, function_tool, trace, ItemHelpers
+from agents import Agent, Runner, function_tool, trace, ItemHelpers, RunContextWrapper
 
 from chinese_zodiac import get_chinese_zodiac
 from pydantic import BaseModel
@@ -31,6 +31,13 @@ birthyear = 1989
 western_zodiac = get_western_zodiac(birthMonth, birthDay)
 chinese_zodiac = get_chinese_zodiac(birthyear)
 
+mbti = "INTJ"
+
+
+@function_tool
+def get_irony_definition():
+    return "Irony is a literary device that involves a contrast between what is expected and what actually happens. It can be classified into several types, including situational irony, verbal irony, and dramatic irony."
+
 
 voice_agent = Agent(
     name="voice",
@@ -39,6 +46,7 @@ voice_agent = Agent(
         "You're speaking to a human, speak like so.",
     ),    
     model="gpt-4o-mini",
+    tools=[get_irony_definition],
 )
 
 ocean_agent = Agent(
@@ -59,9 +67,18 @@ mbti_agent = Agent(
     output_type=MBTIOutput,
 )
 
+witty_agent = Agent(
+    name="Wit",
+    handoff_description="A witty response agent.",
+    instructions=
+        "You are a witty agent that can make light in the conversation given the context.",
+    model="gpt-4o-mini",
+)
+
+
 
 async def main():
-    msg = "I like to think about how random the world is."
+    msg = "so the Situational Irony Is that I am learning how irony works. can you tell me more about it?"
 
     ocean_result = await Runner.run(ocean_agent, msg)
     mbti_result = await Runner.run(mbti_agent, msg)
@@ -69,9 +86,10 @@ async def main():
     print(f"Ocean: {ocean_result.final_output}")
     print(f"MBTI: {mbti_result.final_output}")
     
-    voice_result = await Runner.run(voice_agent, f"User message: {msg}\n Sentiments: \n Zodiac: {western_zodiac} {chinese_zodiac}\n Ocean: {ocean_result.final_output}\n MBTI: {mbti_result.final_output}")
-    
-    print(f"Voice: {voice_result.final_output}")
+    voice_result = await Runner.run(voice_agent, f"User message: {msg}\n Sentiments: \n Zodiac: {western_zodiac} {chinese_zodiac}\n Ocean: {ocean_result.final_output}\n MBTI: The user is an  {mbti} and asks: {mbti_result.final_output}")
+    witty_result = await Runner.run(witty_agent, f"User message: {msg}\n Sentiments: \n Zodiac: {western_zodiac} {chinese_zodiac}\n Ocean: {ocean_result.final_output}\n MBTI: The user is an  {mbti} and asks: {mbti_result.final_output}")
+
+    print(f"Voice: {witty_result.final_output}")
 
 if __name__ == "__main__":
     asyncio.run(main())
